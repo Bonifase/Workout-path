6ab1068238bdb60a598cd3fc9c9aaf49077ab9a6
Bonifase Orwa <orwabonifase@gmail.com>
2018-10-02 07:38:09 +0300

    implement signup form

--- workout/routes.py
+++ workout/routes.py
@@ -6,6 +6,12 @@ from workout import app
 def home():
     return render_template("index.html")
 
+
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
